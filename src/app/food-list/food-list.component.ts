cc977c97981b28614ed2ff76c6d42117a88285a6
sk <semaria.eyzac@gmail.com>
2018-05-04 13:51:48 -0700

    add the @Input and import it

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input} from '@angular/core';
+import { Food } from '../model/vegan-food.model';
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
+export class FoodListComponent {
+  @Input() childFoodList: Food[];
+  foods: Food[] = [
+    new Food("tofurkey", "tofu", "protein", 450),
+    new Food("lentil -stew", "lentil", "protein", 500),
+    new Food("fried-spinach-mushroom", "spinach & mushroom", "carb", 400),
+    new Food("fruit-salad", "mango & guava", "vitamin C", 200),
+  ];
+
+  dietColor(currentFood){
+    if (currentFood.diet === "protein"){
+      return "bg-danger";
+    } else if (currentFood.diet === "carb") {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
