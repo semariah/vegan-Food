bdc00dc5bab6d25807b81f2467d9bbe73fa4e39d
sk <semaria.eyzac@gmail.com>
2018-05-04 16:28:37 -0700

    create a new food form and import and update its html and new-food.component.ts files accordingly

---
+++ src/app/new-food/new-food.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Food } from '../model/vegan-food.model';
+
+@Component({
+  selector: 'app-new-food',
+  templateUrl: './new-food.component.html',
+  styleUrls: ['./new-food.component.css']
+})
+export class NewFoodComponent {
+  submitForm(name: string, ingredient: string, diet: string, calories: string, organic: boolean) {
+    let newFood: Food = new Food(name, ingredient, diet, parseInt(calories));
+    this.sendFood.emit(newFood);
+  }
+
+  constructor() { }
+
+
+
+}
