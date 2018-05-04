e9398a68d43402fc4c4ed7f77d56d1e29ca9666b
sk <semaria.eyzac@gmail.com>
2018-05-04 13:53:58 -0700

    update the changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Food } from './model/vegan-food.model';
 })
 export class AppComponent {
   title = 'Vegan Food App';
-  foods: Food[] = [
+  masterFoodList: Food[] = [
     new Food("tofurkey", "tofu", "protein", 450),
     new Food("lentil -stew", "lentil", "protein", 500),
     new Food("fried-spinach-mushroom", "spinach & mushroom", "carb", 400),
@@ -21,22 +21,7 @@ export class AppComponent {
     this.selectedFood = clickedFood;
   }
 
-  dietColor(currentFood){
-    if (currentFood.diet === "protein"){
-      return "bg-danger";
-    } else if (currentFood.diet === "carb") {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-
   finishedEditing() {
     this.selectedFood = null;
   }
-
-
-
-
-
-  }
+ }
